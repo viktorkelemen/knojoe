2d6e7b5c9df5c52f2953aa75e89617efea8b4d76
Qihuan Piao <fantasyday@gmail.com>
2012-11-10 01:52:30 +0900

    Setup facebook omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+  helper_method :current_user, :signed_in?
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.nil? ? user : user.id
+  end
+
+  def require_login
+    unless signed_in?
+      session[:return_to] = request.path
+      redirect_to login_path, alert: 'You must be logged in to access this section.'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Knojoe::Application.routes.draw do
+  match '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/failure', to: redirect('/')
+  match '/logout', to: 'sessions#destroy'
+
   root to: 'home#index'
 end
