6fd2d0b34beb905b139d49184eee658ee453a804
Qihuan Piao <fantasyday@gmail.com>
2012-11-10 02:05:14 +0900

    Generate twitter bootstrap layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Knojoe::Application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create'
   match '/auth/failure', to: redirect('/')
   match '/logout', to: 'sessions#destroy'
+  match '/login', to: 'sessions#new'
+  match '/logout', to: 'sessions#destroy'
 
   root to: 'home#index'
 end
