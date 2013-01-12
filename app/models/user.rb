108c542a01d50bb3702e519fe27363920e58441b
Qihuan Piao <fantasyday@gmail.com>
2013-01-12 16:58:16 +0900

    Set daily push notificaiton limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ActiveRecord::Base
   has_many :messages, foreign_key: :author_id
   has_many :chats
   has_many :asked_questions, class_name: 'Chat', foreign_key: :requester_id
-  has_many :helped_questions, class_name: 'Chat', foreign_key: :responder_id
+  has_many :helped_questions, class_name: 'Chat', foreign_key: :responder_id do
+    def on_today
+      where(created_at: (Date.today.beginning_of_day..Date.today.end_of_day))
+    end
+  end
 
   def self.create_with_omniauth(info)
     create(name: info['name'], image: info['image'], email: info['email'])
   end
 
+  DAILY_PUSH_NOTIFICATION_LIMIT = 10
+
+  def available_for_push_notification?
+    helped_questions.on_today.count <= DAILY_PUSH_NOTIFICATION_LIMIT
+  end
 end
