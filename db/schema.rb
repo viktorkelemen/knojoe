7450f42396b6fb0ebab5f0913bb995c9c8798cb3
Qihuan Piao <fantasyday@gmail.com>
2013-01-12 19:43:58 +0900

    Use delayed_job to handle checking connection timeout on server side

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121208084332) do
+ActiveRecord::Schema.define(:version => 20130112100658) do
 
   create_table "chats", :force => true do |t|
     t.integer  "responder_id"
@@ -21,6 +21,22 @@ ActiveRecord::Schema.define(:version => 20121208084332) do
     t.datetime "started_at"
     t.datetime "finished_at"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "identities", :force => true do |t|
     t.string   "provider"
