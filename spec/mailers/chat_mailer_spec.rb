1c822151b5e640959eafecd3ff1c2e37d88fee09
Qihuan Piao <fantasyday@gmail.com>
2013-06-02 17:54:27 +0900

    Fix broken mailer spec

--- spec/mailers/chat_mailer_spec.rb
+++ spec/mailers/chat_mailer_spec.rb
@@ -1,18 +1,27 @@
 require "spec_helper"
 
 describe ChatMailer do
-  describe "send_conversation" do
-    let(:mail) { ChatMailer.send_conversation }
+  describe '#send_conversation' do
+    let(:mail) { ChatMailer.send_conversation(chat, user, 'to@example.com') }
+    let(:chat) { stub(:chat, id: 1, created_at: Time.now, messages: [stub(content: 'hello', author: user)]) }
+    let(:user) { stub(:user) }
 
-    it "renders the headers" do
-      mail.subject.should eq("Send conversation")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+    it 'sets subject based on I18n' do
+      mail.subject.should eq(I18n.t('chat_mailer.send_conversation.subject'))
     end
 
-    it "renders the body" do
-      mail.body.encoded.should match("Hi")
+    it 'sends to specified recipient' do
+      mail.to.should eq(['to@example.com'])
     end
+
+    it 'includes review chat url in mail body' do
+      mail.body.encoded.should include(review_chat_url(chat.id))
+    end
+
+    it 'includes chat message' do
+      mail.body.encoded.should include('hello')
+    end
+
   end
 
 end
