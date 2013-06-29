1e02eb52f36597eff8e93ddaa077b24f8fde41f4
Viktor Kelemen <kelemen.viktor@gmail.com>
2013-06-30 00:40:49 +0100

    Add two last_message helpers and replace last_sentence_from_each_side

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -21,18 +21,40 @@ describe Chat do
     end
   end
 
-  describe '#last_sentence_from_each_side' do
-    it 'grabs last sentence from requester and responder' do
-      requester = create(:requester)
-      responder = create(:responder)
-      chat = create(:chat, requester: requester, responder: responder)
+  describe 'last message helpers' do
+    let(:requester) { create(:requester) }
+    let(:responder) { create(:responder) }
+    let(:chat) { create(:chat, requester: requester, responder: responder) }
 
-      message_1 = create(:message, author: requester, chat: chat)
-      system_message_1 = create(:system_message, chat: chat)
-      message_2 = create(:message, author: responder, chat: chat)
-      message_3 = create(:message, author: requester, chat: chat)
+    describe 'requester' do
+      it 'grabs the last message' do
+        message_1 = create(:message, author: requester, chat: chat)
+        system_message_1 = create(:system_message, chat: chat)
+        message_2 = create(:message, author: responder, chat: chat)
+        message_3 = create(:message, author: requester, chat: chat)
 
-      expect(chat.last_sentence_from_each_side).to eq([message_3.content, message_2.content])
+        expect(chat.last_message_from_requester).to eq(message_3.content)
+      end
+
+      it 'returns nil if there is no message besides the initial question' do
+        expect(chat.last_message_from_requester).to be_nil
+      end
+    end
+
+    describe 'responder' do
+      it 'grabs the last message' do
+        message_1 = create(:message, author: requester, chat: chat)
+        system_message_1 = create(:system_message, chat: chat)
+        message_2 = create(:message, author: responder, chat: chat)
+        message_3 = create(:message, author: requester, chat: chat)
+
+        expect(chat.last_message_from_responder).to eq(message_2.content)
+      end
+
+      it 'returns nil if there is no message besides the initial question' do
+        expect(chat.last_message_from_responder).to be_nil
+      end
     end
   end
+
 end
