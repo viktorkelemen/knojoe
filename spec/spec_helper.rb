aa5b7f37f78960639c74c58da154a8ae5df92668
Qihuan Piao <fantasyday@gmail.com>
2013-06-30 17:24:07 +0900

    Treat rspec symbols as metadata keys with true values

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,4 +40,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.include UserMacros::Controller, type: :controller
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
